30ba3700075cebd7733f7873729ef24e1b9ca249
Patrick Lavallée <lavalleep@gmail.com>
2018-03-31 10:51:33 -0400

    identation should be spaces and not tabulations. Corrected typo in encoding also

--- question1.py
+++ question1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-9 -*-
+# -*- coding: utf-8 -*-
 """
 Cours GTI100
 Lab 3 - Partie 2
@@ -25,41 +25,41 @@ def question1():
 
     info( '*** Adding hosts\n' )
     h1 = net.addHost('h1', ip='10.0.0.1')
-	h2 = net.addHost('h2', ip='10.0.0.2')
-	h3 = net.addHost('h3', ip='10.0.0.3')
-	h4 = net.addHost('h4', ip='10.0.0.4')
+    h2 = net.addHost('h2', ip='10.0.0.2')
+    h3 = net.addHost('h3', ip='10.0.0.3')
+    h4 = net.addHost('h4', ip='10.0.0.4')
 
     info( '*** Adding switch\n' )
-	s1 = net.addSwitch('s1')
-	s2 = net.addSwitch('s2')
-	s3 = net.addSwitch('s3')
-	s4 = net.addSwitch('s4')
-	s5 = net.addSwitch('s5')
-	s6 = net.addSwitch('s6')
-	s7 = net.addSwitch('s7')
-	s8 = net.addSwitch('s8')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+    s4 = net.addSwitch('s4')
+    s5 = net.addSwitch('s5')
+    s6 = net.addSwitch('s6')
+    s7 = net.addSwitch('s7')
+    s8 = net.addSwitch('s8')
 	
     info( '*** Creating links\n' )
     net.addLink(h1, s1)
-	net.addLink(h2, s1)
-	net.addLink(h3, s8)
-	net.addLink(h4, s8)
-	net.addLink(s1, s2)
-	net.addLink(s1, s3)
-	net.addLink(s1, s6)
-	net.addLink(s2, s8)
-	net.addLink(s3, s4)
-	net.addLink(s4, s5)
-	net.addLink(s5, s8)
-	net.addLink(s6, s7)
-	net.addLink(s7, s8)
+    net.addLink(h2, s1)
+    net.addLink(h3, s8)
+    net.addLink(h4, s8)
+    net.addLink(s1, s2)
+    net.addLink(s1, s3)
+    net.addLink(s1, s6)
+    net.addLink(s2, s8)
+    net.addLink(s3, s4)
+    net.addLink(s4, s5)
+    net.addLink(s5, s8)
+    net.addLink(s6, s7)
+    net.addLink(s7, s8)
 	
     info( '*** Starting network\n')
     net.start()
 	
 	
     # votre code...
-			
+    net.cmd('pingall')		
 	
 
 

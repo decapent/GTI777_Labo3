b59f86fdd26eef48a2b6e7d7650e5c6b4bc5c180
Patrick Lavallée <lavalleep@gmail.com>
2018-04-03 09:42:20 -0400

    Encapsulated the topology creation mecanism within the Labo3Topology class.

--- question1.py
+++ question1.py
@@ -13,65 +13,70 @@ from mininet.node import OVSSwitch, Controller, RemoteController
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
+from mininet.topo import Topo
 from time import time
+from time import sleep
 
 
+class Labo3Topology(Topo):
+    def __init__(self, nbHosts, nbSwitches, linkbw, **params):
+        
+        # Base class initialization
+        Topo.__init__(self, **params)
+        
+        # Creating hosts
+        hosts = [self.addHost('h%s' % h, ip='10.0.0.%s' % h) 
+            for h in range(1, nbHosts+1)]
+        
+        # Creating switches
+        switches = [self.addSwitch('s%s' % s) 
+            for s in range(1, nbSwitches+1)] 
+        
+        # Creating links
+        self.addLink(hosts[0], switches[0], bw=linkbw, delay='5ms')
+        self.addLink(hosts[1], switches[0], bw=linkbw, delay='4ms')
+        self.addLink(hosts[2], switches[7], bw=linkbw, delay='6ms')
+        self.addLink(hosts[3], switches[7], bw=linkbw, delay='3ms')
+        self.addLink(switches[0], switches[1], bw=linkbw, delay='12ms')
+        self.addLink(switches[0], switches[2], bw=linkbw, delay='3ms')
+        self.addLink(switches[0], switches[5], bw=linkbw, delay='5ms')
+        self.addLink(switches[1], switches[7], bw=linkbw, delay='4ms')
+        self.addLink(switches[2], switches[3], bw=linkbw, delay='6ms')
+        self.addLink(switches[3], switches[4], bw=linkbw, delay='3ms')
+        self.addLink(switches[4], switches[7], bw=linkbw, delay='2ms')
+        self.addLink(switches[5], switches[6], bw=linkbw, delay='10ms')
+        self.addLink(switches[6], switches[7], bw=linkbw, delay='1ms')
 
 def question1():
+    # Creating the experiment's topology
+    topology = Labo3Topology(nbHosts=4, nbSwitches=8, linkbw=10)
+    
     # creating the Mininet object which supports an external controller
-    net = Mininet(controller=RemoteController, link=TCLink)
-
+    net = Mininet(topo=topology, controller=RemoteController, link=TCLink)
+    
     info( '*** Adding controller\n' )
+    
     # Connecting the controller to Floodlight Openflow port 
     c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
-
-    info( '*** Adding hosts\n' )
-    # Hosts are created with static IP
-    h1 = net.addHost('h1', ip='10.0.0.1')
-    h2 = net.addHost('h2', ip='10.0.0.2')
-    h3 = net.addHost('h3', ip='10.0.0.3')
-    h4 = net.addHost('h4', ip='10.0.0.4')
-
-    info( '*** Adding switch\n' )
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
-    s3 = net.addSwitch('s3')
-    s4 = net.addSwitch('s4')
-    s5 = net.addSwitch('s5')
-    s6 = net.addSwitch('s6')
-    s7 = net.addSwitch('s7')
-    s8 = net.addSwitch('s8')
-	
-    info( '*** Creating links\n' )
-    # Creating the topology of the whole network under test
-    linkbw = 10
-    net.addLink(h1, s1, bw=linkbw, delay='5ms')
-    net.addLink(h2, s1, bw=linkbw, delay='4ms')
-    net.addLink(h3, s8, bw=linkbw, delay='6ms')
-    net.addLink(h4, s8, bw=linkbw, delay='3ms')
-    net.addLink(s1, s2, bw=linkbw, delay='12ms')
-    net.addLink(s1, s3, bw=linkbw, delay='3ms')
-    net.addLink(s1, s6, bw=linkbw, delay='5ms')
-    net.addLink(s2, s8, bw=linkbw, delay='4ms')
-    net.addLink(s3, s4, bw=linkbw, delay='6ms')
-    net.addLink(s4, s5, bw=linkbw, delay='3ms')
-    net.addLink(s5, s8, bw=linkbw, delay='2ms')
-    net.addLink(s6, s7, bw=linkbw, delay='10ms')
-    net.addLink(s7, s8, bw=linkbw, delay='1ms')
-	
+    
     info( '*** Starting network\n')
     # Starting the experiment
     net.start()
-	
+    print "Initializing the experiment"
+    sleep(2)
+    
     # Testing network connectivity between all hosts
     net.pingAll()
     
     # Launching an iperf command running in background
     # between h1 and h3. Output will be printed to file.
-    performanceCommand = 'iperf -s > out.txt &'
-    h3.cmd(performanceCommand)
-	
-    info( '*** Running CLI\n' )
+    # servercommand = 'iperf –s > server.txt &'
+    # clientcommand = 'iperf -c 10.0.0.3 > client.txt &'
+    # h3.cmd(servercommand)
+    # h1.cmd(clientcommand)
+    
+    # net.iperf((h3, h1))
+    
     CLI( net )
     info( '*** Stopping network' )
     net.stop()
